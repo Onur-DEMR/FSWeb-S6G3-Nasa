8cad00d172874b4b1c7b4c323f2041126eb5a1a3
Onur-DEMR <119036165+Onur-DEMR@users.noreply.github.com>
2023-01-10 13:55:05 +0300

    Genel çerçeve ve dummy data oluşturuldu.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { dummyData } from "./dummyData";
+
+const [dataKullan, setDataKullan] = useState(dummyData);
 
 function App() {
   return (
     <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <h1>{dataKullan.title}</h1>
+    <p>{dataKullan.explanation}</p>
+    <img src={dataKullan.url} alt="" />
+    <div className="date-content">
+    <button>Azalt</button>
+    <p>{dataKullan.date}</p>
+    <button>Arttır</button>
+    </div>
+    <p>{dataKullan.copyright}</p>
     </div>
   );
 }
